4b4837e43cc893389257deecb06ed2c55950f706
rcarteraz <robert.l.carter2@gmail.com>
2022-11-24 23:51:57 -0700

    Update sarcastic_typer.py
    added description of code

--- Xiao RP2040/sarcastic_typer.py
+++ Xiao RP2040/sarcastic_typer.py
@@ -1,3 +1,5 @@
+# This is a simple script for writing sarcastically. It will display a message stating "Normal Typing Mode Engaged" while a switch is in the off position. When the switch is flipped to on, it will change that message to "sArCaStIc TyPiNg MoDe EnGaGeD!" and then send the keycode for CAPS_Lock to the connected computer at an interval of .1 seconds. 
+
 import time
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
